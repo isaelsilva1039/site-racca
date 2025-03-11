e9fb4cf3aa7c3b38b5761d53689549694007afc3
ezequiel8841 <ezequiel_cardoso@live.com>
2025-03-11 07:50:50 -0300

    footer novo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,12 @@ function Footer() {
   return (
     <FooterContainer>
       <p>Racca Saúde © 2025 - Todos os direitos reservados</p>
+      <p>
+        Responsável Técnico: Nathan Henrique Bovo dos Reis - CRP: 06/176856
+      </p>
+      <p>
+        Razão Social: Racca Monitoramento e Gestão da Saúde Ltda - CNPJ: 50.633.829/0001-08
+      </p>
     </FooterContainer>
   );
 }
